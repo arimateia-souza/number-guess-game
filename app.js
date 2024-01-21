8618b72f4ca1ee6862d131226e575ef7c7c2dca5
Ari Souza <email.arisantos@gmail.com>
2024-01-20 22:04:21 -0300

    update

--- app.js
+++ app.js
@@ -2,39 +2,24 @@
 alert('Bem vindo ao jogo de advinhação do numero secreto');
 let numeroScreto =6;
 let chute;
-let chances;
+let tentativas = 1;
 let nome;
-tentativas();
 
-while (chute != numeroScreto && chances > 0) {
-
-    chute = prompt(`${nome}, Digite um numero de 1 a 50 \nNumero de chances: ${chances}`);
+nome = prompt('Digite seu nome');
+while (chute != numeroScreto ) {
+    chute = prompt(`${nome}, Digite um numero de 1 a 50 \nTentativa: ${tentativas}`);
     if(chute == numeroScreto){
-        alert(`Acertou, numero secreto: ${numeroScreto}`);
-    }else {
-        chances--;
-        if (chute > numeroScreto ) {
-            alert(`Errou, tente novamente! \nDica: Número secreto Menor que ${chute}`);
-        }else{
-            alert(`Errou, tente novamente!\nDica: Número secreto Maior que ${chute}`);
-        }   
+        let palavraTentativa = tentativas > 1 ? 'tentativas' : 'tentativa';
+        alert(`Acertou!!! Numero secreto ${numeroScreto} com ${tentativas} ${palavraTentativa}`);
+    }else {    
+        chute > numeroScreto ? alert(`Errou, tente novamente! \nDica: Número secreto MENOR que ${chute}`): 
+        alert(`Errou, tente novamente!\nDica: Número secreto MAIOR que ${chute}`);  
     }
-    if (chances == 0) {
-        alert(`Sem tentativas!`);
-        tentativas();
- 
-    }
-}
-
-function tentativas(){ 
-    do {
-        nome = prompt('digite o nome do jogador');
-        chances = prompt(`${nome}, digite a quantidade de chances MAIOR que 2 e MENOR que 5`);;
-    } while (chances > 5 || chances < 2);
+    tentativas++;
 }
 
 
-// DESAFIO: HORA DA PRATICA
+
 
 
 

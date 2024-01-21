d2ddcf8e8fc0775f5d791d063bf8823784b4a5be
Ari Souza <email.arisantos@gmail.com>
2024-01-21 18:58:17 -0300

    projeto final

--- app.js
+++ app.js
@@ -1,13 +1,14 @@
 //JOGO DE ADVINHAÇÃO
 alert('Bem vindo ao jogo de advinhação do numero secreto');
-let numeroScreto = parseInt(Math.random() * 100 + 1);
+let numeroMaximo = 100;
+let numeroScreto = parseInt(Math.random() * numeroMaximo + 1);
 let chute;
 let tentativas = 1;
 let nome;
 console.log(numeroScreto);
 nome = prompt('Digite seu nome');
 while (chute != numeroScreto ) {
-    chute = prompt(`${nome}, Digite um numero de 1 a 100 \nTentativa: ${tentativas}`);
+    chute = prompt(`${nome}, Digite um numero de 1 a ${numeroMaximo} \nTentativa: ${tentativas}`);
     if(chute == numeroScreto){
         let palavraTentativa = tentativas > 1 ? 'tentativas' : 'tentativa';
         alert(`Acertou!!! Numero secreto ${numeroScreto} com ${tentativas} ${palavraTentativa}`);
